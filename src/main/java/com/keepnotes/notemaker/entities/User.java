47cb7e5bebf575f6ec7f5efd87fc4ac77f8a668d
ashishsaxena16108 <118245347+ashishsaxena16108@users.noreply.github.com>
2023-09-02 05:17:04 +0000

    modified:   src/main/java/com/keepnotes/notemaker/entities/User.java 	modified:   target/classes/com/keepnotes/notemaker/entities/User.class

--- src/main/java/com/keepnotes/notemaker/entities/User.java
+++ src/main/java/com/keepnotes/notemaker/entities/User.java
@@ -18,7 +18,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int    user_id;
     private String name;
-    
     private String email;
     private String password;
     private String image;
